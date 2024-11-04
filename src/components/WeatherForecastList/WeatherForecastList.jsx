d072d564ec0e66440112afac03f0f6c2c6605306
loly1710 <loly15710@gmail.com>
2024-11-04 13:03:57 +0300

    Finish Lab

---
+++ src/components/WeatherForecastList/WeatherForecastList.jsx
@@ -0,0 +1,13 @@
+const WeatherForecastsList = (props) => {
+    const {weatherForecast} = props
+    return (
+      <div className="weather">
+  <h2>{weatherForecast.day}</h2>
+  <img src={weatherForecast.img} alt={weatherForecast.imgAlt} />
+  <p><span>conditions: </span>{weatherForecast.conditions} </p>
+  <p><span>time: </span>{weatherForecast.time}</p>
+  </div>
+    );
+  };
+  
+  export default WeatherForecastsList;
